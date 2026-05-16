16da1f5669e6a151e251b2c1b8b19ef3ea2ee9f0
Lore0 <lorenzopanizzolo04@gmail.com>
2026-05-16 10:52:27 +0200

    feat: main, different routes

--- app/src/main/java/com/example/simon/MainActivity.kt
+++ app/src/main/java/com/example/simon/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -26,25 +27,44 @@ class MainActivity : ComponentActivity() {
             SimonTheme {
                 val navController = rememberNavController()
                 val hist = rememberSaveable { mutableStateListOf<String>() }
+                val listViewModel: ListViewModel = viewModel()
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     NavHost(
-                        navController = navController, startDestination = "screen1",
+                        navController = navController, startDestination = "list",
                         modifier = Modifier.padding(innerPadding),
                         // diminuita animazione di default (troppo lenta)
                         enterTransition = { fadeIn(animationSpec = tween(150)) },
                         exitTransition = { fadeOut(animationSpec = tween(150)) },
                     ) {
-                        composable("screen1") {
+                        composable("game") {
+                            val gameViewModel: GameViewModel = viewModel()
                             GameScreen(
-                                onRecap = { seq ->
-                                    hist.add(seq)
-                                    navController.navigate("screen2")
+                                viewModel = gameViewModel,
+                                onRecap = {
+                                    // torno nella prima schermata (lista)
+                                    navController.popBackStack()
                                 }
                             )
                         }
-                        composable("screen2") {
-                            ListScreen(hist = hist)
+                        composable("list") {
+                            listViewModel.loadGames()
+                            ListScreen(
+                                viewModel = listViewModel,
+                                onPlayGame = { navController.navigate("game") },
+                                onDetail = { clickedGame ->
+                                    listViewModel.selectedGame = clickedGame
+                                    navController.navigate("detail")
+                                }
+                            )
+                        }
+                        composable("detail") {
+                            val game = listViewModel.selectedGame
+                            if (game != null) {
+                                DetailScreen(game = game)
+                            } else {
+                                navController.popBackStack()
+                            }
                         }
                     }
                 }
