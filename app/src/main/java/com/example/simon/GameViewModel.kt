da450547e58fa72356f10128e5776414f517df0b
Lore0 <lorenzopanizzolo04@gmail.com>
2026-05-25 11:20:26 +0200

    fix: dont keep activity settings-> savedStateHandle

--- app/src/main/java/com/example/simon/GameViewModel.kt
+++ app/src/main/java/com/example/simon/GameViewModel.kt
@@ -6,7 +6,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.example.simon.db.Game
 import com.example.simon.db.GameDatabase
@@ -16,15 +18,31 @@ import kotlinx.coroutines.delay
 enum class GameState {
     READY,       // partita non ancora iniziata
     COMP_TURN,   // il computer mostra la sequenza
-    PLAYER_TURN, // tocca al giocatore
+    PLAYER_TURN, // turno del giocatore
     PAUSED,      // pausa (solo durante COMP_TURN)
     GAME_OVER    // errore o "Fine partita"
 }
 
-class GameViewModel (application: Application) : AndroidViewModel(application) {
+class GameViewModel (
+    application: Application,
+    private val savedStateHandle: SavedStateHandle
+) : AndroidViewModel(application) {
+
+    // init db
+    private val gameDao = GameDatabase.getDatabase(application).gameDao()
+
     // sound
     private val soundPool: SoundPool = SoundPool.Builder().build()
     private val soundMap = mutableMapOf<String, Int>()
+
+    var state by mutableStateOf(savedStateHandle["state"] ?: GameState.READY)
+    var activeColor by mutableStateOf<String?>(savedStateHandle["color"])
+    var actualError by mutableStateOf(savedStateHandle["error"] ?: false)
+    var flashAll by mutableStateOf(false)
+
+    val compSeq = (savedStateHandle["comp"] ?: emptyList<String>()).toMutableStateList()
+    val playerSeq = (savedStateHandle["player"] ?: emptyList<String>()).toMutableStateList()
+    val colors = listOf("R", "G", "B", "Y", "C", "M")
 
     init{
         soundMap["R"] = soundPool.load(application, R.raw.r, 1)
@@ -34,23 +52,23 @@ class GameViewModel (application: Application) : AndroidViewModel(application) {
         soundMap["Y"] = soundPool.load(application, R.raw.y, 1)
         soundMap["C"] = soundPool.load(application, R.raw.c, 1)
         soundMap["game_over"] = soundPool.load(application, R.raw.game_over, 1)
+
+        // TODO: da scrivere specifiche README -> decisione quando ripristino stato
+        if (state == GameState.COMP_TURN) {
+            activeColor = null
+            savedStateHandle["color"] = null
+            // rifacciamo vedere seq da inzio
+            playCompSeq()
+        } else if (state == GameState.PLAYER_TURN) {
+            // utente puó reinserire seq da capo
+            playerSeq.clear()
+            savedStateHandle["player"] = ArrayList(playerSeq)
+        }
     }
-
-    // init db
-    private val gameDao = GameDatabase.getDatabase(application).gameDao()
-    var state by mutableStateOf(GameState.READY)
-    var activeColor by mutableStateOf<String?>(null)
-    var actualError by mutableStateOf(false)
-    var flashAll by mutableStateOf(false)
-
-    val compSeq = mutableStateListOf<String>()
-    val playerSeq = mutableStateListOf<String>()
-    val colors = listOf("R", "G", "B", "Y", "C", "M")
 
     fun startGame() {
         if (state != GameState.READY) return
         compSeq.clear()
-        playerSeq.clear()
         addNewColor()
         playCompSeq()
     }
@@ -67,12 +85,15 @@ class GameViewModel (application: Application) : AndroidViewModel(application) {
 
     private fun addNewColor() {
         compSeq.add(colors.random())
+        savedStateHandle["comp"] = ArrayList(compSeq)
     }
 
     private fun playCompSeq() {
         viewModelScope.launch {
             state = GameState.COMP_TURN
+            savedStateHandle["state"] = state
             playerSeq.clear()
+            savedStateHandle["player"] = ArrayList(playerSeq)
             delay(500)
 
             for (color in compSeq) {
@@ -80,13 +101,16 @@ class GameViewModel (application: Application) : AndroidViewModel(application) {
                 if (state == GameState.GAME_OVER) return@launch
 
                 activeColor = color
+                savedStateHandle["color"] = color
                 playSound(color)
                 delay(600)
                 activeColor = null
+                savedStateHandle["color"] = null
                 delay(300)
             }
 
             state = GameState.PLAYER_TURN
+            savedStateHandle["state"] = state
         }
     }
 
@@ -95,12 +119,15 @@ class GameViewModel (application: Application) : AndroidViewModel(application) {
 
         viewModelScope.launch {
             activeColor = color
+            savedStateHandle["color"] = color
             delay(150)
             activeColor = null
+            savedStateHandle["color"] = null
         }
 
         playSound(color)
         playerSeq.add(color)
+        savedStateHandle["player"] = ArrayList(playerSeq)
 
         val currentIndex = playerSeq.size - 1
         if (playerSeq[currentIndex] == compSeq[currentIndex]) {
@@ -121,8 +148,10 @@ class GameViewModel (application: Application) : AndroidViewModel(application) {
     fun onPause() {
         if (state == GameState.COMP_TURN) {
             state = GameState.PAUSED
+            savedStateHandle["state"] = state
         } else if (state == GameState.PAUSED) {
             state = GameState.COMP_TURN
+            savedStateHandle["state"] = state
         }
     }
 
@@ -130,6 +159,7 @@ class GameViewModel (application: Application) : AndroidViewModel(application) {
     fun handleGameOver(errorIndex: Int = playerSeq.size, isActualError: Boolean = false) {
         if (state == GameState.GAME_OVER) return
         state = GameState.GAME_OVER
+        savedStateHandle["state"] = state
 
         if (isActualError) {
             viewModelScope.launch {
@@ -138,7 +168,11 @@ class GameViewModel (application: Application) : AndroidViewModel(application) {
                 delay(800)
                 flashAll = false
                 actualError = true
+                savedStateHandle["error"] = true
             }
+        } else {
+            actualError = false
+            savedStateHandle["error"] = false
         }
 
         // non salviamo seq da 1
