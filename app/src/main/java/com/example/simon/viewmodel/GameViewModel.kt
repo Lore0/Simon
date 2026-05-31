7d39a6cfd759d8e25ed79a057a952ced485b3ad7
Lore0 <lorenzopanizzolo04@gmail.com>
2026-05-31 17:40:55 +0200

    chore: comment

--- app/src/main/java/com/example/simon/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/example/simon/viewmodel/GameViewModel.kt
@@ -54,7 +54,6 @@ class GameViewModel (
         soundMap["C"] = soundPool.load(application, R.raw.c, 1)
         soundMap["game_over"] = soundPool.load(application, R.raw.game_over, 1)
 
-        // TODO: da scrivere specifiche README -> decisione quando ripristino stato
         if (state == GameState.PAUSED || state == GameState.COMP_TURN || state == GameState.PLAYER_TURN) {
             // rifacciamo vedere seq da inzio (comp_turn)
             // mentre utente riprende da dove si era fermato
